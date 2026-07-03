b2d9300a42e86605022e1b0a4642124dac2a241e
nmccready <creadbot@github.com>
2026-07-02 21:36:11 -0400

    ci: relax footer-max-line-length to 200, matching body limit

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'body-max-line-length': [2, 'always', 200],
+    'footer-max-line-length': [2, 'always', 200],
     'header-max-length': [2, 'always', 200],
     'subject-case': [0],
     'type-enum': [2, 'always', ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test', 'ai']],
